197c316cb09d2f27702a39bf74683a9dd2809d3a
Aakash <Akashgautam10071997@gmail.com>
2017-07-21 15:18:33 +0530

    controller added to class main

--- Class 1/main.js
+++ Class 1/main.js
@@ -7,3 +7,8 @@
 //
 //       });
 // });
+var foodieApp = angular.module('foodieApp',[]);
+    
+    foodieApp.controller('mainController',function($scope){
+       $scope.restaurants =['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
+    });
