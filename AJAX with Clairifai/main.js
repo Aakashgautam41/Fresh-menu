742f57af57b023af717db1c73cb4b2579e71b47d
Aakash <Akashgautam10071997@gmail.com>
2017-07-23 20:12:45 +0530

    Clarify api is working fine

--- AJAX with Clairifai/main.js
+++ AJAX with Clairifai/main.js
@@ -1,20 +1,26 @@
-window.onload = function() {
-   var data = {"inputs":[{"data":{"image":{"url":"http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG"}}}]}
-      $.ajax({
-          'type': 'POST',
-          'url': 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs',
-          'headers': {
-              'Authorization': 'Key c65a89fe0d7e4500b845cadfb40f8d53',
-              'Content-Type': 'application/json'
-          },
-          'data': data,
-          success: function (response) {
-              console.log(response);
-          },
-          error: function (xhr) {
-              console.log(xhr);
-          }
-      })
+ window.onload = function() {
 
-
+ var data = '{"inputs":[{"data":{"image":{"url":"https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?h=350&auto=compress&cs=tinysrgb"}}}]}'
+    $.ajax({
+        'type': 'POST',
+        'url': 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs',
+        'headers': {
+            'Authorization': 'Key c65a89fe0d7e4500b845cadfb40f8d53',
+            'Content-Type': 'application/json'
+        },
+        'data': data,
+        success: function (response) {
+              var ingredients = response.outputs[0].data.concepts;
+              var list = '';
+                for (var i =0;i < ingredients.length;i++) {
+                 list += '<div class="ingredient">' + ingredients[i].name + '</div>'
+             }
+                 $('.ingredients').html(list);
+          },
+             error: function (xhr) {
+             console.log(xhr);
+        }
+    })
+// Write AJAX call here 
+	
 }
